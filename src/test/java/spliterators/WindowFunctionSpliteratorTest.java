6e137e4fa6b0d9bcead7819a193a82217ef002a2
Krzysiek Przygudzki <kpr@touk.pl>
2018-11-23 10:16:24 +0100

    Extract class GroupingSpliteratorTest to a separate file

--- src/test/java/spliterators/WindowFunctionSpliteratorTest.java
+++ src/test/java/spliterators/WindowFunctionSpliteratorTest.java
@@ -8,39 +8,6 @@ import java.util.stream.StreamSupport;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class GroupingSpliteratorTest {
-
-    @Test
-    void shouldReturnGrouped() {
-        Stream<String> source = Stream.of("a", "b", "c", "d", "e", "f");
-
-        GroupingSpliterator groupingSpliterator = GroupingSpliterator.from(source.spliterator(), 3);
-        Stream<String> stream = StreamSupport.stream(groupingSpliterator, false);
-
-        assertThat(stream).containsExactly("abc", "def");
-    }
-
-    @Test
-    void shouldReturnIncompleteGroupIfNotEnoughElementsAvailableUpstream() {
-        Stream<String> source = Stream.of("a", "b", "c", "d", "e");
-
-        GroupingSpliterator groupingSpliterator = GroupingSpliterator.from(source.spliterator(), 3);
-        Stream<String> stream = StreamSupport.stream(groupingSpliterator, false);
-
-        assertThat(stream).containsExactly("abc", "de");
-    }
-
-    @Test
-    void shouldReturnEmptyGroupIfUpstreamIsEmpty() {
-        Stream<String> source = Stream.of();
-
-        GroupingSpliterator groupingSpliterator = GroupingSpliterator.from(source.spliterator(), 3);
-        Stream<String> stream = StreamSupport.stream(groupingSpliterator, false);
-
-        assertThat(stream).isEmpty();
-    }
-}
-
 class WindowFunctionSpliteratorTest {
 
     @Test
